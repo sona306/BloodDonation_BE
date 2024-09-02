425065b8bea2d146e2a05072432f7176e4c7d09b
sona306 <sonasabu307@gmail.com>
2024-09-02 20:45:07 +0530

    admin signup comp

--- app.js
+++ app.js
@@ -3,11 +3,25 @@ const mongoose = require("mongoose")
 const cors = require("cors")
 const bcrypt = require("bcrypt")
 const jwt = require("jsonwebtoken")
+const loginModel = require("./Models/Admin")
 
 let app = express()
 
-app.get("/",(req,res)=>{
-    res.send("hello")
+app.use(express.json())
+app.use(cors())
+
+mongoose.connect("mongodb+srv://sonasabu:sonavi306@cluster0.ejzjjq6.mongodb.net/mernblood_donationdb?retryWrites=true&w=majority&appName=Cluster0")
+
+
+app.post("/adminSignUp",(req,res)=>{
+    let input = req.body
+    let hashedPassword = bcrypt.hashSync(req.body.password,10)
+    console.log(hashedPassword)
+    req.body.password = hashedPassword
+    console.log(input)
+    let result = new loginModel(input)
+    result.save()
+    res.json({"status":"success"})
 })
 
 app.listen(8080,()=>{
