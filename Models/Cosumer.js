ca39940047d778728e6ae6a8b1c03f06e23ccff3
sona306 <sonasabu307@gmail.com>
2024-09-07 21:09:56 +0530

    consumer login comp

---
+++ Models/Cosumer.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose")
+
+const consumerLoginSchema = mongoose.Schema(
+    {
+        username:{type:String,require:true},
+        fullname:{type:String,require:true},
+        dateofbirth:{type:String,require:true},
+        gender:{type:String,require:true},
+        bloodtype:{type:String,require:true},
+        phonenumber:{type:String,require:true},
+        email:{type:String,require:true},
+        homeaddress:{type:String,require:true},
+        location:{type:String,require:true},
+        medicalhistory:{type:String,require:true},
+        lastdonationdate:{type:String,require:true},
+        hospitalname:{type:String,require:true},
+        emergencycontactnumber:{type:String,require:true},
+        urgencylevel:{type:String,require:true},
+        password:{type:String,require:true},
+        confirm:{type:String,require:true}    
+    }
+)
+
+const consumerloginModel = mongoose.model("consumerlogin",consumerLoginSchema)
+module.exports=consumerloginModel
--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bcrypt = require("bcrypt")
 const jwt = require("jsonwebtoken")
 const loginModel = require("./Models/Admin")
 const donarloginModel = require("./Models/Donar")
+const consumerloginModel = require("./Models/Cosumer")
 
 
 let app = express()
@@ -89,6 +90,55 @@ app.post("/donarSignUp",(req,res)=>{
 app.post("/donarSignIn",(req,res)=>{
     let input = req.body
     let result = donarloginModel.find({username:input.username}).then(
+        (response)=>{
+            if (response.length>0) {
+                const validator=bcrypt.compareSync(input.password,response[0].password)
+                if (validator) {
+                    jwt.sign({email:input.username},"blood-donation",{expiresIn:"2d"},
+                        (error,token)=>{
+                            if (error) {
+                                res.json({"status" : "token creation failed"})
+                            } else {
+                                res.json({"status" : "success","token":token})
+                            }
+                        })
+                } else {
+                    res.json({"status" : "incorect password"})
+                }
+            } else {
+                res.json({"status" : "username doesnt exist"})
+            }
+        }
+    )
+})
+
+//CONSUMER SIGN UP
+app.post("/consumerSignUp",(req,res)=>{
+    let input = req.body
+    let hashedPassword = bcrypt.hashSync(req.body.password,10)
+    //console.log(hashedPassword)
+    let hashedConfirm = bcrypt.hashSync(req.body.confirm,10)
+    req.body.password = hashedPassword
+    req.body.confirm = hashedConfirm
+
+    consumerloginModel.find({ email: req.body.email }).then(
+        (items) => {
+            if (items.length > 0) {
+                res.json({ "status": "Email id already exits" })
+            } else {
+                let result = new consumerloginModel(input)
+                result.save()
+                res.json({ "status": "success" })
+            }
+        }
+    )
+})
+
+//CONSUMER SIGN IN
+
+app.post("/consumerSignIn",(req,res)=>{
+    let input = req.body
+    let result = consumerloginModel.find({username:input.username}).then(
         (response)=>{
             if (response.length>0) {
                 const validator=bcrypt.compareSync(input.password,response[0].password)
